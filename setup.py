47d76b9f0f92e208357e3060e43fc33b5ee59319
vestigialcode <dibyajiban232@gmail.com>
2022-03-05 18:15:26 +0530

    changed

--- setup.py
+++ setup.py
@@ -1,13 +1,13 @@
 import logging
-from kiteconnect import KiteTicker
-
+import kiteconnect
+# just for checking import gone right
 logging.basicConfig(level=logging.DEBUG)
 
 # Initialise
-kws = KiteTicker("your_api_key", "your_access_token")
+#kws = KiteTicker("your_api_key", "your_access_token")
 
 print("No problem.. you can proceed")
 
 # Infinite loop on the main thread. Nothing after this will run.
 # You have to use the pre-defined callbacks to manage subscriptions.
-kws.connect()
+#kws.connect()
